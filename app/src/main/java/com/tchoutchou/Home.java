6495c3ea001a1d45737d566be81d86bb55dfa654
Ludovic-972 <ludovicmathurin1@gmail.com>
2022-02-20 21:30:08 +0100

    Modification de la page d'accueil

--- app/src/main/java/com/tchoutchou/Home.java
+++ app/src/main/java/com/tchoutchou/Home.java
@@ -1,12 +1,13 @@
 package com.tchoutchou;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,14 +16,19 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
-import com.tchoutchou.utils.ExternalDB;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import com.tchoutchou.util.Towns;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class Home extends AppCompatActivity {
 
@@ -34,58 +40,45 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        if(!hasActiveInternetConnection()){ // Si la connexion à la base de données a échouée
-            /*Renvoyer vers la page hors connexion*/
-        }
+
+
 
         departureTown = findViewById(R.id.departureTown);
         arrivalTown = findViewById(R.id.arrivalTown);
         departureTime = findViewById(R.id.departureTime);
+        Button goToRides = findViewById(R.id.toRides);
 
         init();
 
         departureTime.setOnClickListener(view -> {
             Calendar calendar = Calendar.getInstance();
-            TimePickerDialog timePickerDialog = new TimePickerDialog(Home.this, (timePicker, hourOfDay, minutes) -> {
-                departureTime.setText(String.format("%02d:%02d", hourOfDay, minutes));
-            }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true);
+            TimePickerDialog timePickerDialog = new TimePickerDialog(
+                    Home.this,
+                    (timePicker, hourOfDay, minutes) -> departureTime.setText(String.format("%02d:%02d", hourOfDay, minutes)),
+                    calendar.get(Calendar.HOUR_OF_DAY),
+                    calendar.get(Calendar.MINUTE),
+                    true);
             timePickerDialog.show();
         });
 
-        Button goToRides = findViewById(R.id.toRides);
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                getApplicationContext(),
+                android.R.layout.simple_dropdown_item_1line,
+                Towns.getTowns());
 
+        departureTown.setAdapter(adapter);
+        arrivalTown.setAdapter(adapter);
 
-        goToRides.setOnClickListener(view -> {
+        goToRides.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
         });
     }
 
-    private boolean isNetworkAvailable() {
-        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-        return activeNetworkInfo != null;
-    }
-
-
-    public boolean hasActiveInternetConnection() {
-        if (isNetworkAvailable()) {
-            try {
-                HttpURLConnection urlc = (HttpURLConnection) (new URL("http://www.google.com").openConnection());
-                urlc.setRequestProperty("User-Agent", "Test");
-                urlc.setRequestProperty("Connection", "close");
-                urlc.setConnectTimeout(1500); urlc.connect();
-                return (urlc.getResponseCode() == 200); }
-            catch (IOException e) {
-                Log.e("InternetConnexion", "Error checking internet connection", e);
-            }
-        } else {
-            Log.e("InternetConnexion", "No network available!");
-        } return false;
-    }
-
-
-
+    @SuppressLint("SetTextI18n")
     public void init(){
         SharedPreferences preferences = getSharedPreferences("userInfos", MODE_PRIVATE);
         this.greetings = findViewById(R.id.greetings);
@@ -94,18 +87,24 @@ public class Home extends AppCompatActivity {
         String text = greetings.getText().toString();
 
         greetings.setText(text+" "+username+"\uD83D\uDC4B,");
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_dropdown_item_1line, ExternalDB.getAllTowns());
-
-        departureTown.setAdapter(adapter);
-        arrivalTown.setAdapter(adapter);
-
-
-
 
     }
 
+    private boolean isNetworkConnected(){
+        try {
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo[] networkInfo = null;
 
+            if (connectivityManager != null) {
+                networkInfo = connectivityManager.getAllNetworkInfo();
+            }
+            Log.d("InternetConnection","Device is connected to Internet");
+            return networkInfo != null & networkInfo[0].isConnected();
+        }catch(NullPointerException e){
+            Log.e("InternetConnection","Device is not connected to Internet");
+            return false;
+        }
+    }
 
 
 }
